cfbda39db683d1aa8ce71e8d4400f56ca2c27a9c
Ali Ünal <0033484@gmail.com>
2021-09-23 01:44:44 +0300

    Ragdoll components selection added

---
+++ Assets/Scripts/Ragdoll/Editor/RagdollEditor.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace AngryKoala.Ragdoll
+{
+    [CustomEditor(typeof(Ragdoll))]
+    public class RagdollEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            Ragdoll ragdoll = (Ragdoll)target;
+
+            DrawDefaultInspector();
+
+            if(GUILayout.Button("Select Ragdoll Components"))
+            {
+                List<Object> objects = new List<Object>();
+                foreach(var ragdollComponent in ragdoll.RagdollComponents)
+                {
+                    objects.Add(ragdollComponent.gameObject);
+                }
+
+                Selection.objects = objects.ToArray();
+            }
+        }
+    }
+}
