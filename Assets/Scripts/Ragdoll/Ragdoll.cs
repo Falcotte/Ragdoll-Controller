f7726f3fcdb8b75c2f4deb9e24bddaaf659d1b89
Ali Ünal <0033484@gmail.com>
2021-09-23 01:22:36 +0300

    Push methods added

--- Assets/Scripts/Ragdoll/Ragdoll.cs
+++ Assets/Scripts/Ragdoll/Ragdoll.cs
@@ -27,5 +27,33 @@ namespace AngryKoala.Ragdoll
                 ragdollComponents[i].ComponentCollider.enabled = false;
             }
         }
+
+        // Push ragdoll components with an identical force
+        public void PushRagdoll(Vector3 force)
+        {
+            for(int i = 0; i < ragdollComponents.Count; i++)
+            {
+                ragdollComponents[i].ComponentRigidbody.AddForce(force, ForceMode.Impulse);
+            }
+        }
+
+        // Push entire ragdoll with a force that originates from originPosition (simplified version of PushRagdollComponents)
+        public void PushRagdoll(float force, Vector3 originPosition)
+        {
+            Vector3 forceVector = (transform.position - originPosition).normalized * force;
+            for(int i = 0; i < ragdollComponents.Count; i++)
+            {
+                ragdollComponents[i].ComponentRigidbody.AddForce(forceVector, ForceMode.Impulse);
+            }
+        }
+
+        // Push ragdoll components with a force that originates from originPosition
+        public void PushRagdollComponents(float force, Vector3 originPosition)
+        {
+            for(int i = 0; i < ragdollComponents.Count; i++)
+            {
+                ragdollComponents[i].ComponentRigidbody.AddForce((ragdollComponents[i].transform.position - originPosition).normalized * force, ForceMode.Impulse);
+            }
+        }
     }
 }
